dd62aea55c7f394aaf12e27bd67566fcdaaef60b
Sulaiman Adewale <hackerslord96@gmail.com>
2025-04-03 08:50:43 +0100

    Update Termii SMS Driver to use new API endpoint and improve payload structure; add VSCode settings and update dependencies

--- build/index.js
+++ build/index.js
@@ -5,19 +5,22 @@ const http_1 = require("@avanda/http");
 class TermiiSmsDriver extends app_1.SMS.SmsDriver {
     async send(msg) {
         var _a;
-        let TERMII_API_KEY = app_1.Env.get('TERMII_API_KEY');
+        let TERMII_API_KEY = app_1.Env.get("TERMII_API_KEY");
         let request = new http_1.Request();
         let payload = {
-            "to": msg.to,
-            "from": msg.from,
-            "sms": msg.body,
-            "type": "plain",
-            "channel": "dnd",
-            "api_key": TERMII_API_KEY,
+            to: msg.to,
+            from: msg.from,
+            sms: msg.body,
+            type: "plain",
+            channel: msg.channel || "dnd",
+            api_key: TERMII_API_KEY,
         };
         request.setData(payload);
+        /**
+         * https://v3.api.termii.com
+         */
         try {
-            let response = await request.post("https://api.ng.termii.com/api/sms/send");
+            let response = await request.post("https://v3.api.termii.com/api/sms/send");
             console.log({ response, payload });
             if (response.statusCode > 299)
                 throw new Error((_a = response.data) === null || _a === void 0 ? void 0 : _a.message);
--- index.ts
+++ index.ts
@@ -1,39 +1,42 @@
-import { SMS, Env } from "@avanda/app"
-import { Request } from "@avanda/http"
+import { SMS, Env } from "@avanda/app";
+import { Request } from "@avanda/http";
 interface Msg {
-    to: string,
-    from?: string,
-    body: string
+  to: string;
+  from?: string;
+  body: string;
+  channel?: string;
 }
 
 export default class TermiiSmsDriver extends SMS.SmsDriver {
-    async send(msg: Msg): Promise<boolean> {
+  async send(msg: Msg): Promise<boolean> {
+    let TERMII_API_KEY = Env.get<string>("TERMII_API_KEY");
 
-        let TERMII_API_KEY = Env.get<string>('TERMII_API_KEY')
+    let request = new Request();
+    let payload = {
+      to: msg.to,
+      from: msg.from,
+      sms: msg.body,
+      type: "plain",
+      channel: msg.channel || "dnd",
+      api_key: TERMII_API_KEY,
+    };
 
-        let request = new Request()
-        let payload = {
-            "to": msg.to,
-            "from": msg.from,
-            "sms": msg.body,
-            "type": "plain",
-            "channel": "dnd",
-            "api_key": TERMII_API_KEY,
-        }
+    request.setData(payload);
 
-        request.setData(payload)
+    /**
+     * https://v3.api.termii.com
+     */
 
-
-        try {
-            let response = await request.post("https://api.ng.termii.com/api/sms/send")
-            console.log({ response, payload })
-            if (response.statusCode > 299)
-                throw new Error(response.data?.message);
-            return true;
-        } catch (e) {
-            console.error(e);
-            return false;
-        }
+    try {
+      let response = await request.post(
+        "https://v3.api.termii.com/api/sms/send"
+      );
+      console.log({ response, payload });
+      if (response.statusCode > 299) throw new Error(response.data?.message);
+      return true;
+    } catch (e) {
+      console.error(e);
+      return false;
     }
-
+  }
 }
